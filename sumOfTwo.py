68f6cd14f3a7ddfd0ae7fbd145bb89521c8445b1
Vedanth Narayanan <narayave@oregonstate.edu>
2018-01-17 16:27:30 -0800

    Sum of two needs to be faster

--- sumOfTwo.py
+++ sumOfTwo.py
@@ -2,31 +2,36 @@
 # Common Techniques: Basic
 # sumOfTwo
 
-
 def sumOfTwo(a, b, v):
 
-    a = sorted(a)
-    b = sorted(b)
+	if a == [] or b == []:
+		return False
 
-    if len(a) < len(b):
-        size = len(b)
-    else:
-        size = len(a)
-    
-    # Trying out greedy algorithm here
-    for i in xrange(size-1, 0, -1):
-        leftover = abs(v - b[i])
-        print leftover, a
-        if leftover in a:
-            return True
-    return False
-	
-if __name__ == '__main__':
+	a = sorted(a)
+	b = sorted(b)
 
-	a = [4, 6, 4, 2, 9, 6, 6, 2, 9, 2]
-	b = [3, 4, 5, 1, 4, 10, 9, 9, 6, 4]
-	v = 5
-	print sumOfTwo(a, b, v)
-	
-	# Working for most tests, but supposedly failing 9.
+	# Trying out greedy algorithm here
+	if len(a) < len(b):
+		size = len(b)
+		for i in xrange(size-1, -1, -1):
+			leftover = v - b[i]
+			if leftover <= 0:
+				pass
+			# print leftover, a
+			if leftover in a:
+				return True
+		return False
+	else:
+		size = len(a)
+		for i in xrange(size-1, -1, -1):
+			leftover = v - a[i]
+			if leftover <= 0:
+				pass
+			# print leftover, a
+			if leftover in b:
+				return True
+		return False
+
+
+	# Working for most tests, but supposedly failing 6.
 	# Execution error that I don't know about.
