2659737d6b78e8e212535760c77ea58350beaaf2
Vedanth Narayanan <vedanth.nd@gmail.com>
2020-12-05 17:58:50 -0800

    rotLeft

---
+++ rotLeft.py
@@ -0,0 +1,38 @@
+# HackerRank - https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the rotLeft function below.
+
+
+def rotLeft(n, a, d):
+
+    rot = d % n
+    print(rot, a[rot:], a[:rot])
+    res = a[rot:] + a[:rot]
+    return res
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(n, a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
