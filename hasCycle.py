9d9331d5c13b66db9c8a12769cfbb0541c5e6e49
Vedanth Narayanan <vedanth.nd@gmail.com>
2021-02-03 15:10:46 -0800

    had cycle

--- hasCycle.py
+++ hasCycle.py
@@ -27,3 +27,31 @@ class Solution:
             curr = curr.next
 
         return False
+
+
+# O(1) solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+
+        if not head:
+            return
+        if not head.next:
+            return False
+
+        p1 = head
+        p2 = head.next
+
+        while p2 and p2.next:
+            print(p1.val, p2.val)
+            if p1 == p2:
+                return True
+            p1 = p1.next
+            p2 = p2.next.next
+
+        return False
